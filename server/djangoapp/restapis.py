f3f8644148d8761bacfc4b5c9dcda327cfa12554
chiliLime <jaimeson17@gmail.com>
2022-12-15 14:41:39 -0500

    more troubleshooting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,15 +13,27 @@ import time
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
-    print(kwargs)
+    
+    # If argument contain API KEY
+    api_key = kwargs.get("api_key")
     print("GET from {} ".format(url))
     try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
                                     params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
+
     status_code = response.status_code
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
@@ -69,26 +81,16 @@ def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 def get_dealer_by_id_from_cf(url, id):
-    results = []
     json_result = get_request(url, id=id)
-    #print('get dealer by id', json_result)
+    print('json_result from get by id',json_result)
     if json_result:
-        dealers = json_result
-        for dealer_doc in dealers:
-            #print('dealer_doc: ', dealer_doc)
-            dealer_obj = CarDealer(
-                address=dealer_doc["address"],
-                city=dealer_doc["city"],
-                full_name=dealer_doc["full_name"],
-                id=dealer_doc["id"],
-                lat=dealer_doc["lat"],
-                long=dealer_doc["long"],
-                short_name=dealer_doc["short_name"],
-                st=dealer_doc["st"],
-                zip=dealer_doc["zip"]
-            )
-            results.append(dealer_obj)
-    return results
+        dealers = json_result["body"]
+        dealer_doc = dealers[0]
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                        id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"], 
+                        full_name=dealer_doc["full_name"],st=dealer_doc["st"], zip=dealer_doc["zip"], 
+                        short_name=dealer_doc["short_name"])
+    return dealer_obj
 
 def get_dealer_reviews_from_cf(url, **kwargs):
     results = []
